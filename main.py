25c2beca1d92aef8d51404b4ba83c0bd7a7b6b88
daylight55 <jojomasanori3@gmail.com>
2023-03-04 02:19:30 +0900

    準備完了

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # テスト用
-with open("./data/test.txt", "r", encoding="utf-8") as f:
-  words = [word for word in f.read().splitlines()]
+# with open("./data/test.txt", "r", encoding="utf-8") as f:
+#   words = [word for word in f.read().splitlines()]
 
-# with open("./data/input.txt", "r", encoding="utf-8") as f:
-#     words = [ word for word in f.read().splitlines() ]
+with open("./data/input.txt", "r", encoding="utf-8") as f:
+  words = [word for word in f.read().splitlines()]
 
 
 # ChatGPT APIにリクエストを送信して結果を取得する
@@ -21,8 +21,8 @@ for word in words:
   prompt = f'''
     {word}はネットワーク用語です。
     {word}について2048文字程度のWikipedia風解説記事をMarkdownで書いてください。
-    構成は、対象の用語と正称を見出し1、その後に「特徴」・「応用例」・「注意点」・「関連用語」をそれぞれ見出し2のセクションに分けて書いてください。
-    セクションごとに空行を開けて、見出し1は#、見出し2は##で書いてください。また、文字は左寄せで書いてください。指定したもの以外の見出しはつけないでください。
+    構成は、対象の用語と正称を見出し1、その後に「特徴」・「応用例」・「注意点」・「関連用語」をそれぞれ見出し2のセクションに分けて書いてください。特徴を丁寧に書いてください。
+    また、セクションごとに空行を開けて、見出し1は#、見出し2は##で書いてください。また、文字は左寄せで書いてください。指定したセクション以外の見出しはつけないでください。
   '''
 
   response = openai.Completion.create(
@@ -38,7 +38,7 @@ for word in words:
 
   result = response.choices[0].text
 
-  print(result)
+  print(result, end="\n\n")
 
   # 変数にファイルパスが含まれる場合変換する
   word = word.replace("/", "_")
